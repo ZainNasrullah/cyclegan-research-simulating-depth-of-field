f2b84a26cf40069614afc3fb2a0cbb282dfa660b
ZainNasrullah <zain.nasrullah.zn@gmail.com>
2018-03-24 20:23:40 -0400

    acquired data (not public yet), processed, and tested instance segmentation

---
+++ dataset-preparation/prepare_data.py
@@ -0,0 +1,54 @@
+import os
+from PIL import Image
+import argparse
+
+
+def main(args):
+
+    # load arguments
+    resize_to = (args.size, args.size)
+    input_dir = args.input
+    out_dir = args.output
+
+    # load files and track which ones are visited
+    images = os.listdir(input_dir)
+    visited = []
+
+    for file in images:
+
+        # open image
+        path_to_file = os.path.join(input_dir, file)
+        im = Image.open(path_to_file)
+
+        # store resolution and file size
+        file_size = os.path.getsize(path_to_file)
+        resolution = im.size
+
+        # skip if the image resolution and exact file size has been seen before
+        unique_image = resolution + (file_size,)
+        if unique_image not in visited and file_size > 100000:
+            visited.append(unique_image)
+        else:
+            print("duplicate found:", file)
+            continue
+
+        # resize to specified size
+        im.thumbnail(resize_to, Image.ANTIALIAS)
+        im.save(os.path.join(out_dir, file), "JPEG")
+
+    print("done")
+
+
+if __name__ == "__main__":
+
+    # provide arguments for target resize, input and output directories
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--size', type=int, default=256)
+
+    parser.add_argument(
+        '-i', '--input', default=r"..\get-dataset-flickr\bokeh_downloads")
+    parser.add_argument('-o', '--output', default=r"bokeh")
+
+    args = parser.parse_args()
+
+    main(args)
