c5d4bbc8d450ce503d58b281914bfd5559fda35a
ZainNasrullah <zain.nasrullah.zn@gmail.com>
2018-04-08 03:19:04 -0400

    fixed bugs with running test script

--- pytorch-CycleGAN-and-pix2pix/scripts/test_cyclegan.sh
+++ pytorch-CycleGAN-and-pix2pix/scripts/test_cyclegan.sh
@@ -1,1 +1,5 @@
-python test.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan --phase test --no_dropout
+python test.py --dataroot ./datasets/selfie2bokeh --name selfie2bokeh_cyclegan --model cycle_gan --phase test --no_dropout
+
+# python test.py --dataroot ./datasets/selfie2bokeh --name selfie2bokeh_cyclegan_mask_loss --model cycle_gan --phase test --no_dropout --lambda_mask=1.0
+
+# python test.py --dataroot ./datasets/selfie2bokeh --name selfie2bokeh_cyclegan_mask_added --model cycle_gan --phase test --no_dropout --add_mask
