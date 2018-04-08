69dedfb4f49c1c80f09225411d04a902be3c96e6
ZainNasrullah <zain.nasrullah.zn@gmail.com>
2018-04-08 00:53:03 -0400

    fixed submodule bug

---
+++ PyTorch-Transfer-Learning-of-VGG19-for-Cifar-10-Dataset/README.md
@@ -0,0 +1,52 @@
+# PyTorch Transfer Learning of VGG19 on Cifar-10 Dataset
+Transfer Learning of VGG19 trained using ImageNet an retrained for Cifar-10 Dataset using PyTorch.
+
+## Requirement
+**Library** | **Version**
+--- | ---
+**Python** | **^2.7**
+**PyTorch** | **^0.1.12** 
+**Numpy** | **^1.12.0** 
+**Pickle** |  *  
+
+## Usage
+### Download code:
+```sh
+git clone https://github.com/eugenelet/PyTorch-Transfer-Learning-of-VGG19-for-Cifar-10-Dataset
+
+cd PyTorch-Transfer-Learning-of-VGG19-for-Cifar-10-Dataset
+```
+
+### Train cnn:
+Batch size: 128
+
+Prediction made on per epoch basis. 
+
+161 epochs takes about 3h on GTX 1080.
+
+#### Retrain Model:
+```sh
+python train.py
+```
+
+#### Make prediction:
+##### Using Model Zoo
+```sh
+python test.py
+```
+
+##### Using vgg19.npy
+```sh
+python test_vgg19.py
+```
+
+## Tensorboard
+```sh
+tensorboard --logdir=./tensorboard
+```
+
+## License
+[Apache License 2.0](https://github.com/eugenelet/PyTorch-Transfer-Learning-of-VGG19-for-Cifar-10-Dataset/blob/master/LICENSE)
+
+## Implementation Details
+[My Blog](https://embedai.wordpress.com/2017/07/30/transfer-learning-of-vgg19-on-cifar-10-dataset-using-pytorch/)
---
+++ PyTorch-Transfer-Learning-of-VGG19-for-Cifar-10-Dataset/test_vgg19.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import utils
+import argparse
+
+vgg19_npy_path = './vgg19.npy'
+
+
+# Model
+class VGGnet(nn.Module):
+    def __init__(self):
+        super(VGGnet, self).__init__()
+        params_dict = np.load(vgg19_npy_path, encoding='latin1').item()
+        # Conv 1
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1_1.weight.data = torch.FloatTensor(params_dict['conv1_1'][0]).permute(3, 2, 0, 1).contiguous()
+        self.conv1_1.bias.data = torch.FloatTensor(params_dict['conv1_1'][1])
+
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1_2.weight.data = torch.FloatTensor(params_dict['conv1_2'][0]).permute(3, 2, 0, 1).contiguous()
+        self.conv1_2.bias.data = torch.FloatTensor(params_dict['conv1_2'][1])
+
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+
+        # Conv 2
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2_1.weight.data = torch.FloatTensor(params_dict['conv2_1'][0]).permute(3, 2, 0, 1).contiguous()
+        self.conv2_1.bias.data = torch.FloatTensor(params_dict['conv2_1'][1])
+
+        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2_2.weight.data = torch.FloatTensor(params_dict['conv2_2'][0]).permute(3, 2, 0, 1).contiguous()
+        self.conv2_2.bias.data = torch.FloatTensor(params_dict['conv2_2'][1])
+
+        self.pool2 = nn.MaxPool2d(2, stride=2)
+
+        # Conv 3
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv3_1.weight.data = torch.FloatTensor(params_dict['conv3_1'][0]).permute(3, 2, 0, 1).contiguous()
+        self.conv3_1.bias.data = torch.FloatTensor(params_dict['conv3_1'][1])
+
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv3_2.weight.data = torch.FloatTensor(params_dict['conv3_2'][0]).permute(3, 2, 0, 1).contiguous()
+        self.conv3_2.bias.data = torch.FloatTensor(params_dict['conv3_2'][1])
+
+    def forward(self, x):
+        # First hidden layer
+        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv1_2(x))
+        x = self.pool1(x)
+
+        x = F.relu(self.conv2_1(x))
+        x = F.relu(self.conv2_2(x))
+        x = self.pool2(x)
+
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+
+        return x
+
+
+model = VGGnet()
+model.cuda()
+model.eval()
+
+#image = utils.load_image(args.test)
+#image = image[:, :, ::-1]
+#VGG_MEAN = np.array([103.939, 116.779, 123.68])
+#image = (image * 255.0) - VGG_MEAN
+#image = image.transpose(2, 0, 1)
+#image = image.astype(np.float32)
+#input = torch.from_numpy(image)
+#input = input.cuda()
+#input_var = torch.autograd.Variable(input, volatile=True)
+
+output = model(input_var.unsqueeze(0))
+#output = output.data.cpu().numpy()
+#out = torch.autograd.Variable(torch.from_numpy(output))
+#utils.print_prob(F.softmax(out).data.numpy()[0], './synset.txt')
